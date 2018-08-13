befde7b9db6089bd7e9ec0da306d82a584c060ca
henrique <henrymdays@hotmai.com>
2018-08-13 20:48:56 +0100

    initial commit

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options) { }
+        
+
+        public DbSet<Value> Values{get;set;}
+    }
+}
---
+++ API/Models/Value.cs
@@ -0,0 +1,10 @@
+namespace API.Models
+{
+    public class Value
+    {
+        public int id{get;set;}
+      
+        public string Name{ get;set;}
+        
+    }
+}
