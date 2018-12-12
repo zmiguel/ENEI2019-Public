4aeb53bf6dc09f764b7b013a5ab46ac8142f1d3f
ZMiguel Alves <zmiguel.alves@gmail.com>
2018-12-12 16:43:20 +0000

    Event Models and QRs

---
+++ api/Models/Event.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace api.Models
+
+{
+    public class Event
+    {
+        public int Id{get;set;}
+        public string Nome{get;set;}
+        public string Desc{get;set;}
+    }
+}
