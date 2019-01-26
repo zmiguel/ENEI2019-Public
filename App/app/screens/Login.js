e6f6544df074908b4f887ce67f6acd83e1b95e5b
João Borges <jprb14@gmail.com>
2019-01-26 15:06:32 +0000

    Login by qr code scanner

--- App/app/screens/Login.js
+++ App/app/screens/Login.js
@@ -1,13 +1,16 @@
 import React, {Component, Fragment} from 'react'
-import {View, Text, TextInput, Image, ScrollView} from 'react-native'
-import {RkTheme, RkButton, RkTextInput, RkText} from 'react-native-ui-kitten'
-
+import {View, Image, Vibration, Dimensions} from 'react-native'
+import Icon from "react-native-vector-icons/Ionicons";
 import {UtilStyles} from '../assets/styles'
-import {Validate} from '../Helpers/Validation'
 
 import axios from 'axios';
 import deviceStorage from '../services/deviceStorage';
 
+import QRCodeScanner from 'react-native-qrcode-scanner';
+
+
+const SCREEN_HEIGHT = Dimensions.get("window").height;
+const SCREEN_WIDTH = Dimensions.get("window").width;
 
 export default class Login extends Component {
 
@@ -17,119 +20,100 @@ export default class Login extends Component {
 
     constructor(props) {
         super(props);
-
-        this.state = {
-
-            formValid: false,
-
-            email: '',
-            emailError: false,
-            emailErrorMessage: '',
-
-            password: '',
-            passwordError: false,
-            passwordErrorMessage: '',
-
-            loading: false
-        };
-
-
     }
 
 
-    _validatelogin = (email, password) => {
-        let valid = null;
+    onSuccess = (e) => {
+        console.log(e.data)
 
-        let v = Validate('email', email);
-        this.setState({emailError: v[0], emailErrorMessage: v[1]});
 
-        v = Validate('password', password);
 
-        // setState is asynchronous and so trying to work with state directly after a setState
-        // call won't work as the update won't necessarily have run. Instead you can use the second argument to setState which is a callback
-        this.setState({passwordError: v[0], passwordErrorMessage: v[1]}, function () {
-
-            console.log('Email error: ' + this.state.emailError + '  Pass Error: ' + this.state.passwordError);
-
-        });
-
-        if (this.state.emailError && this.state.passwordError)
-            this.setState({formValid: false});
-        else
-            this.setState({formValid: true});
     };
 
-
-    login() {
-        const {email, password, formValid} = this.state;
-        this.setState({loading: true});
-
-        this._validatelogin(email, password);
-
-        console.log(formValid);
-
-        if (formValid) {
-
-            axios.post("https://reqres.in/api/login", {
-
-                email: email,
-                password: password
-
-            }) //https://reqres.in/api/login email: email, password: password,
-                .then(response => {
-                    console.log(response)
-                    // Save Token
-                    deviceStorage.saveItem('userToken', response.data.token);
-
-                })
-                .catch((error) => {
-                    console.log(error)
-                });
-        }
-    }
-
     render() {
-        //const { email, password, error, loading } = this.state;
-
         return (
-            <ScrollView>
-                <View style={[UtilStyles.section, {backgroundColor: RkTheme.current.colors.input}]}>
-                    <Image style={UtilStyles.loginImage}
-                           source={require('../assets/img/logo.png')}
-                    />
+            <QRCodeScanner
+                showMarker
+                onRead={this.onSuccess.bind(this)}
+                cameraStyle={{ height: SCREEN_HEIGHT }}
+                customMarker={
+                    <View style={styles.rectangleContainer}>
+                            <View style={styles.topOverlay}>
+                                <Image style={UtilStyles.loginImage}
+                                       source={require('../assets/img/logo.png')}
+                                />
+                        </View>
 
-                    <View style={UtilStyles.rowContainer}>
-                        <View style={{flex: 1}}>
-                            <RkTextInput rkType='rounded' style={this.state.emailError ? UtilStyles.errorInput : ''}
-                                         placeholder='Login' keyboardType='email-address'
-                                         onChangeText={(email) => {
-                                             this.setState({email: email});
-                                         }}
-                                         value={this.state.email}
-                            />
-                            {this.state.emailError &&
-                            <RkText style={UtilStyles.errorMsg}>{this.state.emailErrorMessage}</RkText>}
-                            <RkTextInput
-                                secureTextEntry={true} style={this.state.passwordError ? UtilStyles.errorInput : ''}
-                                rkType='rounded' placeholder='Password'
-                                onChangeText={(pass) => {
-                                    this.setState({password: pass});
-                                }}
-                                value={this.state.password}
+                        <View style={{ flexDirection: "row" }}>
+                            <View style={styles.leftAndRightOverlay}>
+                            </View>
 
-                            />
-                            {this.state.passwordError &&
-                            <RkText style={UtilStyles.errorMsg}>{this.state.passwordErrorMessage}</RkText>}
+                            <View style={styles.rectangle}>
+                                <Icon
+                                    name="ios-qr-scanner"
+                                    size={SCREEN_WIDTH}
+                                    color={"#fff"}
+                                />
+                            </View>
+
+
+                            <View style={styles.leftAndRightOverlay}>
+                            </View>
+                        </View>
+
+                        <View style={styles.bottomOverlay}>
                         </View>
                     </View>
-                    <RkButton style={{marginTop: 30}} rkType='rounded xlarge' onPress={() => this.login()}>
-                        Login
-                    </RkButton>
-                </View>
-            </ScrollView>
-
+                }
+            />
         );
     }
 }
+
+
+
+const rectDimensions = SCREEN_WIDTH * 0.85; // this is equivalent to 255 from a 393 device width
+
+const overlayColor = 'rgba(0,0,0,0.85)';
+
+const styles = {
+    rectangleContainer: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "transparent"
+    },
+
+    rectangle: {
+        height: rectDimensions,
+        width: rectDimensions,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "transparent"
+    },
+
+    topOverlay: {
+        flex: 1,
+        height: SCREEN_HEIGHT,
+        width: SCREEN_WIDTH,
+        backgroundColor: overlayColor,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+
+    bottomOverlay: {
+        flex: 1,
+        height: SCREEN_HEIGHT,
+        width: SCREEN_WIDTH,
+        backgroundColor: overlayColor,
+        paddingBottom: SCREEN_WIDTH * 0.25
+    },
+
+    leftAndRightOverlay: {
+        height: rectDimensions,
+        width: SCREEN_WIDTH,
+        backgroundColor: overlayColor
+    },
+};
 
 
