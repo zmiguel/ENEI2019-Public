7ec314be33362a3a4a2ba91c499b22ec31d549bf
henrydays <henrymfdays@gmail.com>
2019-03-19 03:03:56 +0000

    up

--- api/Controllers/CromosController.cs
+++ api/Controllers/CromosController.cs
@@ -26,45 +26,50 @@ namespace api.Controllers
             this.context = context;
             _mapper = mapper;
         }
-        
+
         // GET api/cromos/QR
         // GET cromos do user QR
         [HttpGet("{QR}")]
         public async Task<IActionResult> GetCromos(string QR)
         {
-            int soma=0;
-            var usr = await context.Users.FirstOrDefaultAsync(u=>u.QRcode == QR);
+            int soma = 0;
+            var usr = await context.Users.FirstOrDefaultAsync(u => u.QRcode == QR);
             string[] usrCromos = usr.cromos.Substring(1).Split(",");
             Console.WriteLine(usrCromos[0]);
             var allCromos = await context.Cromos.ToListAsync();
 
             List<Cromos> rList = new List<Cromos>();
-            
-            allCromos.ForEach(delegate(Cromos c){
 
-                for(int i=0;i<usrCromos.Length;i++){
-                    
-                    Boolean found = false;
-                    if(Int32.Parse(usrCromos[i])==c.Id){
-                        soma+=c.pontos;
-                        Cromos toAdd = new Cromos{Id = c.Id,Nome=c.Nome,DescMostrar=c.DescUnlocked,QRCode=c.QRCode,img=c.img, unlocked=true, websiteCromo=c.websiteCromo,pontos=c.pontos, logo=c.logo};
+            allCromos.ForEach(delegate (Cromos c)
+            {
+                Boolean found = false;
+                for (int i = 0; i < usrCromos.Length; i++)
+                {
+
+
+                    if (Int32.Parse(usrCromos[i]) == c.Id)
+                    {
+                        soma += c.pontos;
+                        Cromos toAdd = new Cromos { Id = c.Id, Nome = c.Nome, DescMostrar = c.DescUnlocked, QRCode = c.QRCode, img = c.img, unlocked = true, websiteCromo = c.websiteCromo, pontos = c.pontos, logo = c.logo };
                         rList.Add(toAdd);
-                        found=true;
+                        found = true;
 
                     }
-                    if(!found){
-                       
 
-                        Cromos toAdd = new Cromos{Id = c.Id,Nome=c.Nome,DescMostrar=c.DescLocked,QRCode=c.QRCode,img=c.img , unlocked=false, websiteCromo=c.websiteCromo,pontos=c.pontos};
-                        rList.Add(toAdd);
-                    
-                    }
+                }
+                if (!found)
+                {
+
+
+                    Cromos toAdd = new Cromos { Id = c.Id, Nome = c.Nome, DescMostrar = c.DescLocked, QRCode = c.QRCode, img = c.img, unlocked = false, websiteCromo = c.websiteCromo, pontos = c.pontos };
+                    rList.Add(toAdd);
+
                 }
             });
 
-        cromosToReturn a= new cromosToReturn();
-        a.cromos= rList;
-        a.pontuacao=soma;
+            cromosToReturn a = new cromosToReturn();
+            a.cromos = rList;
+            a.pontuacao = soma;
             return Ok(a);
         }
 
