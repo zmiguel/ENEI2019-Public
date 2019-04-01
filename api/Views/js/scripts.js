8bf918f7a553874eb4403c5dbcd5907e422b2ec2
henrydays <henrymfdays@gmail.com>
2019-04-01 15:50:08 +0100

    up

--- api/Views/js/scripts.js
+++ api/Views/js/scripts.js
@@ -175,37 +175,3 @@ $(window).scroll(function () {
 
 });
 
-function openPage(pageName, elmnt, color) {
-    // Hide all elements with class="tabcontent" by default */
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Remove the background color of all tablinks/buttons
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].style.backgroundColor = "";
-      tablinks[i].style.color = "";
-      tablinks[i].style.borderTop= "0px";
-      tablinks[i].style.fontWeight= "";
-      
-
-    }
-  
-    // Show the specific tab content
-    document.getElementById(pageName).style.display = "block";
-  
-    // Add the specific color to the button used to open the tab content
-    elmnt.style.backgroundColor = color;
-    elmnt.style.color="#CC1A17";
-    elmnt.style.borderTop= "2px solid #cc1a17";
-    elmnt.style.borderRadius="2px"
-    elmnt.style.fontWeight="bold"
-  }
-  
-  // Get the element with id="defaultOpen" and click on it
-  document.getElementById("defaultOpen").click();
-
-
